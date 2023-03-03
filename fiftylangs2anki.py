eca65c6671d1559a573e191c21bac324b9d6c218
Stephen Stammen <sms0066@uah.edu>
2023-03-03 15:51:23 -0600

    Completed issue #1, tags are added by category

--- fiftylangs2anki.py
+++ fiftylangs2anki.py
@@ -34,7 +34,106 @@ os.makedirs(AUDIO_DIR, exist_ok=True)
 os.makedirs(SENTENCES_DIR, exist_ok=True)
 
 LESSON_MAP = {
-    
+    1: "People",
+    2: "Family Members",
+    3: "Getting to Know Others",
+    4: "At School",
+    5: "Countries and Languages",
+    6: "Reading and Writing",
+    7: "Numbers",
+    8: "The Time",
+    9: "Days of the Week",
+    10: "Yesterday-Today-Tomorrow",
+    11: "Months",
+    12: "Beverages",
+    13: "Activities",
+    14: "Colors",
+    15: "Fruits and Food",
+    16: "Seasons and Weather",
+    17: "Around the House",
+    18: "House Cleaning",
+    19: "In the Kitchen",
+    20: "Small Talk 1",
+    21: "Small Talk 2",
+    22: "Small Talk 3",
+    23: "Learning Foriegn Languages",
+    24: "Appointment",
+    25: "In the City",
+    26: "In Nature",
+    27: "In the Hotel - Arrival",
+    28: "In the Hotel - Complaints",
+    29: "At the Restaurant 1",
+    30: "At the Restaurant 2",
+    31: "At the Restaurant 3",
+    32: "At the Restaurant 4",
+    33: "At the Train Station",
+    34: "On the Train",
+    35: "At the Airport",
+    36: "Public Transportation",
+    37: "En Route",
+    38: "In the Taxi",
+    39: "Car Breakdown",
+    40: "Asking for Directions",
+    41: "Where is ...?",
+    42: "City Tour",
+    43: "At the Zoo",
+    44: "Going Out in the Evening",
+    45: "At the Cinema",
+    46: "In the Discotheque",
+    47: "Preparing a Trip",
+    48: "Vacation Activities",
+    49: "Sports",
+    50: "In the Swimming Pool",
+    51: "Running Errands",
+    52: "In the Department Store",
+    53: "Shops",
+    54: "Shopping",
+    55: "Working",
+    56: "Feelings",
+    57: "At the Doctor",
+    58: "Parts of the Body",
+    59: "At the Post Office",
+    60: "At the Bank",
+    61: "Ordinal Numbers",
+    62: "Asking Questions 1",
+    63: "Asking Questions 2",
+    64: "Negation 1",
+    65: "Negation 2",
+    66: "Possessive Pronouns 1",
+    67: "Possessive Pronouns 2",
+    68: "Big-Small",
+    69: "To Need or Want To",
+    70: "To Like Something",
+    71: "To Want Something",
+    72: "To Have to Do Something/Must",
+    73: "To Be Allowed To",
+    74: "Asking for Something",
+    75: "Giving Reasons 1",
+    76: "Giving Reasons 2",
+    77: "Giving Reasons 3",
+    78: "Ajectives 1",
+    79: "Ajectives 2",
+    80: "Ajectives 3",
+    81: "Past Tense 1",
+    82: "Past Tense 2",
+    83: "Past Tense 3",
+    84: "Past Tense 4",
+    85: "Questions-Past Tense 1",
+    86: "Questions-Past Tense 2",
+    87: "Past Tense of Modal Verbs 1",
+    88: "Past Tense of Modal Verbs 2",
+    89: "Imperative 1",
+    90: "Imperative 2",
+    91: "Subordinate Clauses: That 1",
+    92: "Subordinate Clauses: That 2",
+    93: "Subordinate Clauses: If",
+    94: "Conjunctions 1",
+    95: "Conjunctions 2",
+    96: "Conjunctions 3",
+    97: "Conjunctions",
+    98: "Double Connectors",
+    99: "Genitive",
+    100: "Adverbs"
 }
 
 def sentences_file_for_lang(lang: str) -> str:
@@ -142,6 +241,7 @@ def add_note(
     dest_sentence: str,
     filename2: str,
     lesson_link_html: str,
+    note_tags: list,
 ):
     note = genanki.Note(
         model=model,
@@ -151,7 +251,7 @@ def add_note(
             f"[sound:{filename2}]",
             lesson_link_html,
         ],
-        tags=["extracted-bengali", "lesson-title"], # TODO: Add tags for the notes about what it is i.e. 1=BN-People
+        tags=note_tags,
         guid=genanki.guid_for(src, dest, sound_id),
         due=len(deck.notes),
     )
@@ -192,6 +292,8 @@ def generate_deck(
         lesson_link = LESSON_LINK.format(src=src, dest=dest, lesson=page_number)
         lesson_link_html = f'<a href="{lesson_link}">{lesson_link}</a>'
         sentences_1, sentences_2 = get_cached_lesson_sentences(src, dest, str(i))
+        lesson_tag = LESSON_MAP.get(i).replace(" ", "-")
+        note_tags = [f"{src}-to-{dest}", lesson_tag]
         if sentences_1 and sentences_2:
             for sound_id, src_sentence in sentences_1.items():
                 dest_sentence = sentences_2[sound_id]
@@ -207,6 +309,7 @@ def generate_deck(
                     dest_sentence,
                     filename2,
                     lesson_link_html,
+                    note_tags,
                 )
         else:
             sentences_1 = {}
@@ -247,6 +350,7 @@ def generate_deck(
                             dest_sentence,
                             filename2,
                             lesson_link_html,
+                            note_tags,
                         )
                         sentences_1[sound_id] = src_sentence
                         sentences_2[sound_id] = dest_sentence
